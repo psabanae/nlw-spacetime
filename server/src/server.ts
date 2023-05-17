7c1c77c591b5ccad64e9bd33791af9d6475f5d43
psabanae <psabanae3@gmail.com>
2023-05-17 00:39:57 -0400

    Initial CRUD for memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,16 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
-
-
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
-const prisma = new PrismaClient();
 
+app.register(cors, {
+    origin: true,
+})
+app.register(memoriesRoutes)
 
 app.listen({
     port: 3333,
 }).then(() => {
     console.log('Servidor rodando na porta 3333');
 });
-
-app.get('/users', async () => {
-    const users = await prisma.user.findMany()
-
-    return users
-})

81e37249d322997f36a510b217bfc33e07258447
psabanae <psabanae3@gmail.com>
2023-05-19 19:03:07 -0400

    Added memories route to mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useAuthRequest, makeRedirectUri } from 'expo-auth-session';
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
-import { styled } from 'nativewind';
 import * as SecureStore from 'expo-secure-store';
 
-import { useFonts, Roboto_400Regular, Roboto_700Bold,  } from '@expo-google-fonts/roboto';
-import { BaiJamjuree_700Bold,  } from '@expo-google-fonts/bai-jamjuree';
 
-
-import blurBg from '../src/assets/bg-blur.png';
-import Stripes from '../src/assets/stripes.svg';
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg';
 import { api } from '../src/lib/api';
 
 
-const StyledStripes = styled(Stripes);
 
 const discovery = {
     authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -27,12 +19,6 @@ const discovery = {
 export default function App() {
 
     const router = useRouter()
-
-    const [hasLoadedFonts] = useFonts({
-        Roboto_400Regular,
-        Roboto_700Bold,
-        BaiJamjuree_700Bold
-    })
 
     const [ request, response, signInWithGithub ] = useAuthRequest({
     clientId: '3667a58d3d32a156e389',
@@ -63,17 +49,9 @@ export default function App() {
         }
     })
 
-    if (!hasLoadedFonts) {
-        return null
-    }
 
     return (
-        <ImageBackground
-            source={blurBg}
-            className='px-8 py-10 relative bg-gray-900 flex-1 items-center'
-            imageStyle={{ position: 'absolute', left: '-100%' }}
-        >
-            <StyledStripes className='absolute left-2'/>
+        <View className='px-8 py-10 flex-1 items-center'>
 
             <View className='flex-1 items-center justify-center gap-6'>
                 <NLWLogo />
@@ -95,7 +73,7 @@ export default function App() {
                 </TouchableOpacity>
             </View>
             <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Feito com 💜 no NLW da Rocketseat</Text>
-            <StatusBar style="light" translucent />
-        </ImageBackground>
+            
+        </View>
 );
 }
